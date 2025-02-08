06b4a985ed484bd60066a855745885a3da5a15ad
manjeet_s <manjeetsharma0796@gmail.com>
2025-02-08 15:15:46 +0530

    portfolio page created

---
+++ script.js
@@ -0,0 +1,7 @@
+// Example JavaScript - You can add functionality later
+
+// For now, a simple alert when the page loads
+window.onload = function() {
+    alert('Welcome to my portfolio!');
+  };
+  
